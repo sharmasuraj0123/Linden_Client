71e40bd59da14952092f7f8c1850df35f9331f57
Unknown <krishna.arjun96@gmail.com>
2018-03-06 00:08:07 -0500

    Added landing page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,107 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Movies from './Movies';
+import TvShows from "./TvShows";
+import { Header, Grid, Input, Menu, Segment } from 'semantic-ui-react';
 import './App.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import MyComponent from './Mycomponent';
 
+
+const marginStyle = {
+  'margin': 20
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [{
+        title: "Kumki",
+        rating: 4,
+        imageUrl: require("./images/kumki.jpeg")
+      },
+      {
+        title: "MS Dhoni",
+        rating: 5,
+        imageUrl: require("./images/dhoni.jpeg")
+      },
+      {
+        title: "Thar Ragnorak",
+        rating: 2,
+        imageUrl: require("./images/thor.jpg")
+      },
+      {
+        title: "Arjun Reddy",
+        rating: 5,
+        imageUrl: require("./images/Reddy.jpeg")
+      },
+      {
+        title: "Orient Express",
+        rating: 1,
+        imageUrl: require("./images/murder.jpeg")
+      },
+      {
+        title: "Rush",
+        rating: 5,
+        imageUrl: require("./images/rush.jpeg")
+      },
+      {
+        title: "Interstellar",
+        rating: 5,
+        imageUrl: require("./images/interstellar.jpeg")
+      },
+      ],
+      tvShows: [{
+        title: "Prison Break",
+        rating: 4,
+        imageUrl: require("./images/prison_break.jpg")
+      },
+      {
+        title: "Pitchers",
+        rating: 5,
+        imageUrl: require("./images/pitcher.png")
+      }
+      ],
+      activeItem: 'movie'
+    };
+  }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Linden</h1>
-        </header>
-        <MuiThemeProvider>
-          <MyComponent />
-        </MuiThemeProvider>
+        <Segment inverted>
+          <Menu inverted pointing secondary>
+            <Menu.Item name='movie' active={this.state.activeItem === 'movie'} onClick={this.handleItemClick} />
+            <Menu.Item name='tvShow' active={this.state.activeItem === 'tvShow'} onClick={this.handleItemClick} />
+            <Menu.Menu position='right'>
+              <Menu.Item>
+                <Input className='icon' icon='search' placeholder='Search...' />
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu>
 
-      </div>
+        </Segment>
+
+        {
+          this.state.activeItem === 'movie' &&
+          (
+            <div style={marginStyle}>
+              <Header as='h2' block attached='top'>Featured Movies</Header>
+              <Segment block attached>
+                <Grid>
+                  <Grid.Row columns={7}>
+                    <Movies className='Movies' movies={this.state.movies} />
+                  </Grid.Row>
+                </Grid>
+              </Segment>
+            </div>
+          )
+        }
+        {
+          this.state.activeItem === 'tvShow' && (
+            <TvShows tvShows={this.state.tvShows} />)
+        }
+      </div >
     );
   }
 }
---
+++ my-app/src/Movies.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Rating, Grid, Card, Icon, Image } from 'semantic-ui-react';
+
+class Movies extends Component {
+  render() {
+    return (
+      this.props.movies.map((movie) =>
+        <Grid.Column>
+          <Card>
+            <Image height='250' width='300' src={movie.imageUrl} />
+            <Card.Content>
+              <Card.Header>
+                {movie.title}
+              </Card.Header>
+            </Card.Content>
+            <Card.Content extra>
+              <Icon name='comment'/>75 Reviews
+            </Card.Content>
+            <Card.Content extra>
+              <Rating disabled={true} icon='star' defaultRating = {movie.rating} maxRating={movie.rating} />
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+      )
+    );
+  }
+}
+
+export default Movies;

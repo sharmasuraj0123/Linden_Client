017f45d542205741353bb57c397f1f910df35d3c
Unknown <krishna.arjun96@gmail.com>
2018-04-16 12:54:24 -0400

    Index.js file

---
+++ my-app/src/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Route from 'react-router-dom/Route';
+import SearchResults from './pages/SearchResults';
+import LoginForm from './components/LoginLayout';
+import RegisterForm from './components/RegisterForm';
+import NotFound from './pages/NotFound';
+import 'semantic-ui-css/semantic.min.css';
+
+const appStyle = {
+    'text-align': 'center'
+};
+
+ReactDOM.render(
+    <Router>
+        <div style={appStyle}>
+            <Switch>
+                <Route path='/' exact render={
+                    () => {
+                        return (<Home />);
+                    }
+                } />
+                <Route path='/search' exact render={
+                    () => {
+                        return (<SearchResults />);
+                    }
+                } />
+                <Route path='/login' exact render={
+                    () => {
+                        return (<LoginForm />);
+                    }
+                } />
+                <Route path='/register' exact render={
+                    () => {
+                        return (<RegisterForm />);
+                    }
+                } />
+                <Route component={NotFound} />
+            </Switch>
+        </div>
+    </Router>
+    , document.getElementById('root')
+);
+
+registerServiceWorker();

1675eee787eb5a4a32235909fb84474f23dd9250
Unknown <krishna.arjun96@gmail.com>
2018-04-15 21:24:57 -0400

    Pagination :)

--- my-app/src/pages/SearchResults.js
+++ my-app/src/pages/SearchResults.js
@@ -14,45 +14,48 @@ class SearchResults extends Component {
         this.state = {
             movies: [],
             page: 1,
+            totalPages: 0,
+            keywords: '',
             resultCount:{
                 'all' : 0,
                 'movies':0,
                 'tvShows':0,
                 'actors':0
             }
-                        
-            
         };
     }
 
-    handleClick(query) { 
-        this.props.history.push('/search?keywords='+this.query+'&page=1');
-        window.location.reload();
+    createSearchRequest(keywords, page) {
+        axios.get('http://localhost:8080/search?keywords=' + keywords + '&page=' + page)
+        .then(function (response) {
+            console.log(response);
+            let movieList = response.data.movies;
+            let resultCountResponse = response.data.resultCount;
+            let listLength = 0;
+            let totalPages = Math.ceil(response.data.resultCount.all / 10.0);
+            
+            movieList.forEach(function (element) {
+                element.year = element.releaseDate.split('-')[0];
+                element.imageURL = require("../images/Logo.png");
+                listLength++;
+            });
+            this.setState({
+                movies: movieList,
+                numberOfmovies: listLength,
+                resultCount: resultCountResponse,
+                totalPages: totalPages,
+                keywords: keywords,
+                page: page
+            });
 
+        }.bind(this));
     }
 
     componentDidMount() {
         const values = queryString.parse(this.props.location.search);
         let keywords = values.keywords.replace(/ /g, '+');
         let page = values.page;
-        axios.get('http://localhost:8080/search?keywords=' + keywords + '&page=' + page)
-            .then(function (response) {
-                console.log(response);
-                let movieList = response.data.movies;
-                let resultCountResponse = response.data.resultCount;
-               // console.log(response.data);
-                let listLength = 0;
-                movieList.forEach(function (element) {
-                    element.year = element.releaseDate.split('-')[0];
-                    element.imageURL = require("../images/Logo.png");
-                    listLength++;
-                });
-                this.setState({
-                    movies: movieList,
-                    numberOfmovies: listLength,
-                    resultCount: resultCountResponse
-                });
-            }.bind(this));
+        this.createSearchRequest(keywords, page);
     }
 
     render() {
@@ -62,7 +65,17 @@ class SearchResults extends Component {
                 <Segment padded >
                     <StepExampleLinkClickable resultCount={this.state.resultCount} />
                     <Movies className='Movies' movies={this.state.movies} />
-                    <Pagination defaultActivePage={this.state.page} totalPages={10} horizontalAlign='middle' />
+                    <Pagination 
+                        onPageChange ={(e, data) => {
+                            this.props.history.push('/search?keywords='+this.state.keywords+'&page='+data.activePage);
+                            this.state.page = data.activePage;
+                            this.createSearchRequest(this.state.keywords, data.activePage);
+                            this.forceUpdate();
+                        }}
+                        defaultActivePage={this.state.page} 
+                        totalPages={this.state.totalPages} 
+                        horizontalAlign='middle' 
+                    />
                 </Segment>
                 <Footer />
             </div>

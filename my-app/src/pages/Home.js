93bc3de9e08519b9f86dcf3a75df0331ae88c9b3
Unknown <krishna.arjun96@gmail.com>
2018-04-16 21:32:41 -0400

    Removed unwanted pics

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -22,138 +22,134 @@ class Home extends Component {
     render() {
         return (
             <Segment raised style={SegmentStyle}>
-                < NavBar/>
+                <NavBar />
                 <List horizontal>
-                               <List.Item> 
-                                <Embed style={{ fontSize:'25px', width:670, height: 400}}
-                                autoplay='true'
-                                brandedUI
-                                id='uMDVa4yoCWw'
-                                placeholder={require("../images/featured.jpg")}
-                                source='youtube'
-                                />
-                                <Header> Blockers</Header>
-                                </List.Item>
+                    <List.Item>
+                        <Embed style={{ fontSize: '25px', width: 670, height: 400 }}
+                            autoplay='true'
+                            brandedUI
+                            id='uMDVa4yoCWw'
+                            placeholder={require("../images/featured.jpg")}
+                            source='youtube'
+                        />
+                        <Header> Blockers</Header>
+                    </List.Item>
 
-                                <List.Item>   
-                                <Embed style={{ fontSize:'25px', width:283, height: 400}}
-                                autoplay='true'
-                                brandedUI
-                                id='coOKvrsmQiI'
-                                placeholder={require("../images/featured2.jpg")}
-                                source='youtube'
-                                />
-                                <Header> Rampage</Header>   
-                                 </List.Item>
-
-                            </List.Item>
-                        </List>
-                        <Divider horizontal style={DividerStyle}> Fall Collections</Divider>
-                        <List horizontal>
-                            <List.Item style={{ paddingRight: '5em' }}>
-                                <DescriptionCard imagesrc={require('../images/VerifiedFall.png')} verticalAlign='top' />
-                            </List.Item>
-                            <List.Item>
-                                <FeaturedCard verticalAlign='middle' />
-                            </List.Item>
-                            <List.Item>
-                                <FeaturedCard />
-                            </List.Item>
-                            <List.Item>
-                                <FeaturedCard />
-                            </List.Item>
-                            <List.Item>
-                                <FeaturedCard />
-                            </List.Item>
-                            <List.Item>
-                                <FeaturedCard />
-                            </List.Item>
-                        </List>
-                        <Divider />
-                        <List horizontal >
-                            <List.Item style={{ paddingRight: '5em' }}>
-                                <DescriptionCard imagesrc={require('../images/Fall.png')} verticalAlign='top' />
-                            </List.Item>
-                            <List.Item >
-                                <FeaturedCard verticalAlign='middle' />
-                            </List.Item>
-                            <List.Item>
-                                <FeaturedCard />
-                            </List.Item>
-                            <List.Item>
-                                <FeaturedCard />
-                            </List.Item>
-                            <List.Item>
-                                <FeaturedCard />
-                            </List.Item>
-                            <List.Item>
-                                <FeaturedCard />
-                            </List.Item>
-                        </List>
-                        <Divider horizontal style={DividerStyle}>News and features</Divider>
-                        <Image.Group size='small'>
-                            <Image src={require('../images/murder.jpeg')} style={{ width: 150, height: 150 }} />
-                            <Image src={require('../images/rush.jpeg')} style={{ width: 150, height: 150 }} />
-                            <Image src={require('../images/thor.jpg')} style={{ width: 150, height: 150 }} />
-                            <Image src={require('../images/dhoni.jpeg')} style={{ width: 150, height: 150 }} />
-                        </Image.Group>
-                        <Divider horizontal style={DividerStyle}>Headlines Today</Divider>
-                        <Feed>
-                            <Feed.Event>
-                                <Feed.Content>
-                                    You added Elliot Fu to the group <a>Coworkers</a>
-                                </Feed.Content>
-                            </Feed.Event>
-                            <Feed.Event>
-                                <Feed.Content>
-                                    You added Elliot Fu to the group <a>Coworkers</a>
-                                </Feed.Content>
-                            </Feed.Event>
-                            <Feed.Event>
-                                <Feed.Content>
-                                    You added Elliot Fu to the group <a>Coworkers</a>
-                                </Feed.Content>
-                            </Feed.Event>
-                            <Feed.Event>
-                                <Feed.Content>
-                                    You added Elliot Fu to the group <a>Coworkers</a>
-                                </Feed.Content>
-                            </Feed.Event>
-                            <Feed.Event>
-                                <Feed.Content>
-                                    You added Elliot Fu to the group <a>Coworkers</a>
-                                </Feed.Content>
-                            </Feed.Event>
-                            <Feed.Event>
-                                <Feed.Content>
-                                    You added Elliot Fu to the group <a>Coworkers</a>
-                                </Feed.Content>
-                            </Feed.Event>
-                            <Feed.Event>
-                                <Feed.Content>
-                                    You added Elliot Fu to the group <a>Coworkers</a>
-                                </Feed.Content>
-                            </Feed.Event>
-                            <Feed.Event>
-                                <Feed.Content>
-                                    You added Elliot Fu to the group <a>Coworkers</a>
-                                </Feed.Content>
-                            </Feed.Event>
-                        </Feed>
-                    </Grid.Column>
-                    <Grid.Column width={4}>
-                        <Segment raised >
-                            <SideBarList title='Opening This week' />
-                        </Segment>
-                        <Segment raised >
-                            <SideBarList title='Coming Soon' />
-                        </Segment>
-                        <Segment raised >
-                            <SideBarList title='Critic Picks' />
-                        </Segment>
-                    </Grid.Column>
-                </Grid>
-                <Footer/>
+                    <List.Item>
+                        <Embed style={{ fontSize: '25px', width: 283, height: 400 }}
+                            autoplay='true'
+                            brandedUI
+                            id='coOKvrsmQiI'
+                            placeholder={require("../images/featured2.jpg")}
+                            source='youtube'
+                        />
+                        <Header> Rampage</Header>
+                    </List.Item>
+                </List>
+                <Divider horizontal style={DividerStyle}> Fall Collections</Divider>
+                <List horizontal>
+                    <List.Item style={{ paddingRight: '5em' }}>
+                        <DescriptionCard imagesrc={require('../images/VerifiedFall.png')} verticalAlign='top' />
+                    </List.Item>
+                    <List.Item>
+                        <FeaturedCard verticalAlign='middle' />
+                    </List.Item>
+                    <List.Item>
+                        <FeaturedCard />
+                    </List.Item>
+                    <List.Item>
+                        <FeaturedCard />
+                    </List.Item>
+                    <List.Item>
+                        <FeaturedCard />
+                    </List.Item>
+                    <List.Item>
+                        <FeaturedCard />
+                    </List.Item>
+                </List>
+                <Divider />
+                <List horizontal >
+                    <List.Item style={{ paddingRight: '5em' }}>
+                        <DescriptionCard imagesrc={require('../images/Fall.png')} verticalAlign='top' />
+                    </List.Item>
+                    <List.Item >
+                        <FeaturedCard verticalAlign='middle' />
+                    </List.Item>
+                    <List.Item>
+                        <FeaturedCard />
+                    </List.Item>
+                    <List.Item>
+                        <FeaturedCard />
+                    </List.Item>
+                    <List.Item>
+                        <FeaturedCard />
+                    </List.Item>
+                    <List.Item>
+                        <FeaturedCard />
+                    </List.Item>
+                </List>
+                <Divider horizontal style={DividerStyle}>News and features</Divider>
+                <Image.Group size='small'>
+                    <Image src={require('../images/murder.jpeg')} style={{ width: 150, height: 150 }} />
+                    <Image src={require('../images/rush.jpeg')} style={{ width: 150, height: 150 }} />
+                    <Image src={require('../images/thor.jpg')} style={{ width: 150, height: 150 }} />
+                    <Image src={require('../images/dhoni.jpeg')} style={{ width: 150, height: 150 }} />
+                </Image.Group>
+                <Divider horizontal style={DividerStyle}>Headlines Today</Divider>
+                <Feed>
+                    <Feed.Event>
+                        <Feed.Content>
+                            You added Elliot Fu to the group <a>Coworkers</a>
+                        </Feed.Content>
+                    </Feed.Event>
+                    <Feed.Event>
+                        <Feed.Content>
+                            You added Elliot Fu to the group <a>Coworkers</a>
+                        </Feed.Content>
+                    </Feed.Event>
+                    <Feed.Event>
+                        <Feed.Content>
+                            You added Elliot Fu to the group <a>Coworkers</a>
+                        </Feed.Content>
+                    </Feed.Event>
+                    <Feed.Event>
+                        <Feed.Content>
+                            You added Elliot Fu to the group <a>Coworkers</a>
+                        </Feed.Content>
+                    </Feed.Event>
+                    <Feed.Event>
+                        <Feed.Content>
+                            You added Elliot Fu to the group <a>Coworkers</a>
+                        </Feed.Content>
+                    </Feed.Event>
+                    <Feed.Event>
+                        <Feed.Content>
+                            You added Elliot Fu to the group <a>Coworkers</a>
+                        </Feed.Content>
+                    </Feed.Event>
+                    <Feed.Event>
+                        <Feed.Content>
+                            You added Elliot Fu to the group <a>Coworkers</a>
+                        </Feed.Content>
+                    </Feed.Event>
+                    <Feed.Event>
+                        <Feed.Content>
+                            You added Elliot Fu to the group <a>Coworkers</a>
+                        </Feed.Content>
+                    </Feed.Event>
+                </Feed>
+                <Grid.Column width={4}>
+                    <Segment raised >
+                        <SideBarList title='Opening This week' />
+                    </Segment>
+                    <Segment raised >
+                        <SideBarList title='Coming Soon' />
+                    </Segment>
+                    <Segment raised >
+                        <SideBarList title='Critic Picks' />
+                    </Segment>
+                </Grid.Column>
+                <Footer />
             </Segment>
         );
     }

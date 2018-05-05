632ef99332ba7bf1ef6750114888574e83c4729c
Suraj Sharma <sharmasuraj0123@gmail.com>
2018-05-04 22:24:50 -0400

    Added the Review Card

---
+++ my-app/src/components/MovieDropDownComponent.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Grid,List,Button, Menu } from 'semantic-ui-react';
+
+
+const MovieDropDownComponent = () => (
+
+   
+       
+            <div>
+            <Grid columns>
+                <Grid.columns>
+                </Grid.columns>
+            </Grid>
+            </div>
+            
+        );
+  
+
+export default MovieDropDownComponent;
---
+++ my-app/src/components/ReviewCard.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Card,Button,Image, Icon, Rating } from 'semantic-ui-react';
+
+const description = [
+  'Amy is a violinist with 2 years experience in the wedding industry.',
+  'She enjoys the outdoors and currently resides in upstate New York.',
+].join(' ')
+
+
+class ReviewCard extends Component {
+    render() {
+        return (
+            <Card width={75}>
+                <Card.Content>  
+                    <Card.Header>Matthew</Card.Header>
+                    <Card.Meta>  
+                    <Rating defaultRating={3} maxRating={5} disabled />
+                    </Card.Meta>
+                </Card.Content>
+                <Card.Content>
+                    <Card.Content description={description} />   
+                </Card.Content>
+                <Card.Content extra>
+                    <div className='ui two buttons'>
+                        <Button  color='black'>Report</Button> 
+                    </div>          
+                </Card.Content>
+          </Card>
+        );
+    }
+}
+
+export default ReviewCard;

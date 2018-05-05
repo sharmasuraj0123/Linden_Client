72f6e83eaacea33544c3c6cf48671e962ce4a9fc
Suraj Sharma <sharmasuraj0123@gmail.com>
2018-05-05 01:54:56 -0400

    Added a Dropdown to the Navbar

---
+++ my-app/src/components/CastCard.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Card,Button,Image, Icon } from 'semantic-ui-react';
+
+
+
+class CastCard extends Component {
+    render() {
+        return (
+            <Card style={{ width:150, verticalAlign:'bottom'}}>
+            <Image  src={require("../images/testActorImg.jpg")} style={{  height: 150}}/>
+    <Card.Content>
+    <Card.Header>
+        Robert Downey JR
+      </Card.Header>
+    </Card.Content>
+  </Card>
+        )
+    }
+}
+
+export default CastCard;

1719525ed7c4ab50cc1bc7769fa72f23dea41719
Unknown <krishna.arjun96@gmail.com>
2018-05-15 04:22:18 -0400

    Added carousel

--- my-app/src/components/FeaturedMovieCarousal.js
+++ my-app/src/components/FeaturedMovieCarousal.js
@@ -2,6 +2,7 @@
 import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 import React, { Component } from 'react'
 import axios from 'axios';
+import { withRouter, Link } from 'react-router-dom';
 
 var Carousel = require('react-responsive-carousel').Carousel;
 
@@ -10,33 +11,34 @@ class FeaturedMovieCarousal extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
             movies: []
         }
     }
 
     componentDidMount() {
-       
-        axios.get('localhost:8080/movie/featured')
+        axios.get('http://localhost:8080/movie/featured')
             .then(function (response) {
                 let data = response.data;
-               
-             
-                this.setState({         
+                this.setState({
                     movies: data.movies,
-                   
                 });
             }.bind(this));
     }
 
     render() {
-            return (
-                <Carousel showArrows={true} autoPlay={true} infiniteLoop={true} showThumbs={false}>
-                
-                </Carousel>
-            );
-        }
+        return (
+            <Carousel showArrows={true} autoPlay={true} infiniteLoop={true} showThumbs={false}>
+                {this.state.movies.map((movie) =>
+                    <div key={movie.id}>
+                        <img src={movie.photos[0]} />
+                        <Link to={'/movie/' + movie.id}>
+                            <p style={{fontSize:'35px'}} className="legend">{movie.name}</p>
+                        </Link>
+                    </div>
+                )}
+            </Carousel>
+        );
     }
-    
+}
 
-export default FeaturedMovieCarousal;
+export default withRouter(FeaturedMovieCarousal);
